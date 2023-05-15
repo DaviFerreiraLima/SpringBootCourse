4bf3d6ad3ed850e60c6ae3fd10ae6d64c58ed85f
Davi Ferreira Lima <daviferreilima@gmail.com>
2023-05-15 15:00:42 -0300

    adding the inital spring paste

---
+++ mycoolapp/src/main/java/com/springcourse/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -0,0 +1,18 @@
+package com.springcourse.springboot.demo.mycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MycoolappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MycoolappApplication.class, args);
+
+
+
+        System.out.println("hello world");
+    }
+
+}
