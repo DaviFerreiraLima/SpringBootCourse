97644820b5f80db4ec1dd1a17f98062cbb5bf47b
Davi Ferreira Lima <daviferreilima@gmail.com>
2023-05-16 17:51:08 -0300

    adding the lazy init

---
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/compassuol/springcoredemo/rest/DemoController.java
@@ -0,0 +1,26 @@
+package com.compassuol.springcoredemo.rest;
+
+import com.compassuol.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // DEFINE A PRIVATE FIELD FOR THE DEPENDENCY
+
+    private Coach myCoach;
+
+
+   @Autowired
+   public DemoController(Coach theCoach){
+       myCoach= theCoach;
+   }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+       return  myCoach.getDailyWorkout();
+    }
+}
